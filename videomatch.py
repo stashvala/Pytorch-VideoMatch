dadcec2255177c5ae7c70e1c7c97cac67240c313
Stas Hvala <stat.hvala@gmail.com>
2019-01-13 16:05:04 +0100

    Added utils with preprocess and l2 norm, videomatch takes tensors as inputs now, changed cos sim bmm approach to einsum, method for model size in MB, fixed a few things

--- videomatch.py
+++ videomatch.py
@@ -1,33 +1,31 @@
-import numpy as np
-
 import torch
 from torch import nn
 from torchvision.models import resnet101
 from torch.nn.functional import interpolate, softmax
 
+from utils import l2_normalization
 
 class VideoMatch:
-    def __init__(self, reference_img, mask, k=20, out_shape=None, device=None):
+    def __init__(self, ref_t, mask_t, k=20, d=100, out_shape=None, cuda_dev=None):
 
-        self.device = "cpu" if device is None else "cuda:{:d}".format(device)
+        self.device = "cpu" if cuda_dev is None else "cuda:{:d}".format(cuda_dev)
         self.k = k
 
         self.ref_feat = None
         self.mask_fg = None
         self.mask_bg = None
         self.feat_shape = (0, 0)
-        self.out_shape = reference_img.shape if out_shape is None else out_shape
+        self.out_shape = tuple(ref_t.shape[-2:]) if out_shape is None else out_shape
 
-        self.init_vm(reference_img, mask)
+        self.features = self.to_device(Encoder())
+        self.init_vm(ref_t, mask_t)
 
-        self.features = Encoder()
-
-    def init_vm(self, reference_img, mask):
-        self.ref_feat = self.extract_features((reference_img, ))
+    def init_vm(self, ref_t, mask_t):
+        self.ref_feat = self.extract_features(ref_t)
         self.feat_shape = self.ref_feat.shape[2:4]
 
         # added two dimensions to mask since 4D is needed for bilinear interpolation
-        self.mask_fg = torch.from_numpy(mask).unsqueeze(0).unsqueeze(0).float()
+        self.mask_fg = mask_t.unsqueeze(0).unsqueeze(0).float()
         self.mask_bg = (~ self.mask_fg.byte()).float()
 
         # downsample to ref_feat shape, add dim because sim_mat is 5D
@@ -37,16 +35,16 @@ class VideoMatch:
         self.mask_fg, self.mask_bg = self.to_device(self.mask_fg, self.mask_bg)
 
     def to_device(self, *tensors):
-        return tuple(t.cuda(self.device) for t in tensors)
+        t = tuple(t.cuda(self.device) for t in tensors)
+        return t if len(t) > 1 else t[0]
 
-    def extract_features(self, imgs):
-        batch = torch.stack([torch.from_numpy(img) for img in imgs])
-        batch = self.prepare_tensor(batch)
-        return self.features(batch).unbind(0)
+    def extract_features(self, img_t):
+        img_t = img_t if len(img_t.shape) == 4 else img_t.unsqueeze(0)
+        return self.features(self.to_device(img_t))
 
-    def soft_match(self, test_imgs):
+    def soft_match(self, test_t):
 
-        test_feats = self.extract_features(test_imgs)
+        test_feats = self.extract_features(test_t)
         sim_mat = self.cos_similarity(self.ref_feat, test_feats)
         h, w = sim_mat.shape[-2:]
 
@@ -58,21 +56,21 @@ class VideoMatch:
 
         return sim_fg, sim_bg
 
-    def softmax(self, *args):
-        stacked = torch.stack(args)
-        res = softmax(stacked, dim=-1)
-        return res.unbind(0)
 
-    def predict(self, test_imgs):
-        sim_fg, sim_bg = self.soft_match(test_imgs)
+
+    def predict(self, test_t):
+        sim_fg, sim_bg = self.soft_match(test_t)
 
         # upsample similarity response
-        sim_fg = interpolate(sim_fg, size=self.out_shape, mode='bilinear', align_corners=False)
-        sim_bg = interpolate(sim_bg, size=self.out_shape, mode='bilinear', align_corners=False)
+        sim_fg = interpolate(sim_fg.unsqueeze(0), size=self.out_shape, mode='bilinear', align_corners=False)
+        sim_bg = interpolate(sim_bg.unsqueeze(0), size=self.out_shape, mode='bilinear', align_corners=False)
 
-        probs = softmax(sim_fg, sim_bg)
+        fg_prob, bg_prob = self.softmax(sim_fg, sim_bg)
 
-        return probs
+        return fg_prob.squeeze(), bg_prob.squeeze()
+
+    def outlier_removal(self, prev_fg_pred, curr_fg_pred):
+        pass
 
     @staticmethod
     def cos_similarity(X, Y):
@@ -80,26 +78,17 @@ class VideoMatch:
         assert (X.shape[1:] == Y.shape[1:])
 
         # l2 normalization
-        Xnorm = X / torch.norm(X, p=2, dim=-1, keepdim=True)
-        Ynorm = Y / torch.norm(Y, p=2, dim=-1, keepdim=True)
+        Xnorm = l2_normalization(X, dim=-1)
+        Ynorm = l2_normalization(Y, dim=-1)
 
         # compute pairwise similarity between all pairs of features
-        # return torch.einsum("bijk, bilm -> bjklm", Xnorm, Ynorm) # TODO: should work...
+        return torch.einsum("xijk, bilm -> bjklm", Xnorm, Ynorm)
 
-        # fastest alternative to einsum
-
-        _, c, h, w = Xnorm.shape
-        Xnorm = Xnorm.squeeze().view(c, h * w)
-
-        cos_sims = []
-        for batch in Ynorm:
-            batch = batch.view(c, h * w)
-            # computes batched outer product
-            outer = torch.stack([torch.ger(i, j) for i, j in zip(Xnorm, batch)])
-            cs = torch.sum(outer, dim=0).view(h, w, h, w)
-            cos_sims.append(cs)
-
-        return torch.stack(cos_sims)
+    @staticmethod
+    def softmax(*args):
+        stacked = torch.stack(args)
+        res = softmax(stacked, dim=-1)
+        return res.unbind(0)
 
 
 class Encoder(nn.Module):
@@ -118,3 +107,34 @@ class Encoder(nn.Module):
         x = self.feat_ext.layer2(x)
 
         return x
+
+    def weight_MB(self):
+        """
+        :return: Model size in MegaBytes
+        """
+        return sum(p.numel() for p in self.feat_ext.parameters() if p.requires_grad) * 32 / 4 / 2**20
+
+
+if __name__ == '__main__':
+    import sys
+    import numpy as np
+    from PIL import Image
+    import matplotlib.pyplot as plt
+
+    from utils import preprocess
+
+    if len(sys.argv) < 4:
+        raise ValueError("Expected at least three arguments: "
+                         "path to reference image, path to mask, path to test image(s). "
+                         "\nI got: {}".format(sys.argv))
+
+    ref_img = Image.open(sys.argv[1])
+    ref_tensor = preprocess(ref_img)
+    mask_tensor = torch.from_numpy(np.array(Image.open(sys.argv[2])))
+    test_tensors = preprocess(*[Image.open(arg) for arg in sys.argv[3:]])
+
+    vm = VideoMatch(ref_tensor, mask_tensor, out_shape=ref_img.size[::-1], cuda_dev=0)
+
+    for fg, bg in vm.predict(test_tensors):
+        print(fg.shape, bg.shape)
+
